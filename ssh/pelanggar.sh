d345a59e8335514a3759203bb503ba62f3c6bbf8
M RISQI HIDAYAT <79409216+rizkihdyt6@users.noreply.github.com>
2023-06-03 00:57:01 +0700

    Delete ssh directory

--- ssh/pelanggar.sh
+++
@@ -1,7 +0,0 @@
-clear
-figlet -f 3d "User yang melanggar" | lolcat
-cat log-limit.txt
-
-echo "==============================================" | lolcat
-echo "                 CyberVPN TUNNELING"
-echo "==============================================" | lolcat
